71230b7623a314454b1ad3c4f48f918fc36ba8b6
Amos <amosgathungu@gmail.com>
2017-06-09 13:41:33 -0400

    practice exercise from() includes() filter()

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -147,3 +147,13 @@ function bouncer(arr) {
   });
 }
 bouncer([7, "ate", "", false, 9]);
+
+//Seek and Destroy
+function destroyer(arr) {
+
+  var args = Array.from(arguments).slice(1);
+  return arr.filter(function(val) {
+    return !args.includes(val);
+  }); 
+}
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
