3c035d277f14105499c04d08ed26ea7a079af154
Amos <amosgathungu@gmail.com>
2017-06-23 17:50:52 -0400

    HTML entities

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -453,3 +453,22 @@ function uniteUnique(arr){
 
   }
   uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+//HTML entities
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  htmlEntities={
+    '&':'&amp;',
+    '<':'&lt;',
+    '>':'&gt;',
+    '\"':'&quot;',
+    '\'':"&apos;"
+  };
+  //Use map function to return a filtered str with all entities changed automatically.
+  return str.split('').map(function(entity){
+    return htmlEntities[entity] || entity;
+  }).join('');
+}
+
+// test here
+convertHTML("Dolce & Gabbana");
