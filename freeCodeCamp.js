61d55407ccef179b04ff6ebcae300974862a7a80
Amos <amosgathungu@gmail.com>
2017-07-02 23:11:22 -0400

    algorithm practice

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -561,6 +561,7 @@ function dropElements(arr, func) {
 dropElements([1, 2, 3, 4], function(n) {return n >= 3;});
 
 //sides of a triangle
+
 function triangles(n1, n2, n3) {
     function isInvalid(n) {
         return typeof n !== 'number';
@@ -573,5 +574,41 @@ function triangles(n1, n2, n3) {
     return len === 3 ? 'Scalene' :
            len === 2 ? 'Isosceles' : 'Equilateral';
 }
+triangles(2, 3, 4);
 
-triangles(2, 3);
+//Selling stock to make a profit
+function getMaxProfit(arr) {
+	var minIdx = 0;
+    var maxIdx = 1;
+    var currMin = 0;
+    var maxProfit = 0;
+
+    if(arr.length < 2) {
+        throw new Error("Need atleast two time periods to be profitable!");
+    }
+
+    for(var i = 1; i < arr.length; i++) {
+
+        // new current min.
+        if(arr[i] < arr[currMin]) {
+        	currMin = i;
+        }
+
+        // new best profit
+        if(arr[maxIdx] - arr[minIdx] < arr[i] - arr[currMin]) {
+                maxIdx = i;
+            	minIdx = currMin;
+        }
+
+    }
+
+    maxProfit  = arr[maxIdx] - arr[minIdx];
+    return maxProfit;
+}
+
+var arr1 = [10, 7, 5, 8, 11, 9, 1];
+var arr2 = [1];
+console.log(getMaxProfit(arr1));
+// returns 6 (buying for $5 and selling for $11)
+
+//
