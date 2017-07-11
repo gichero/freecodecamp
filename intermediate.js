88f268de7acfffeac6895d26e7aea0c6c12c5b86
Amos <amosgathungu@gmail.com>
2017-07-10 22:31:50 -0400

    algorithm practice

--- intermediate.js
+++ intermediate.js
@@ -461,3 +461,22 @@ function binaryAgent(str) {
 }
 // test here
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+//Everything be true
+function truthCheck(collection, pre) {
+  var arr = [];
+  for (var i=0; i< collection.length; i++){
+    if(!collection[i][pre]){
+      arr.push(collection[i]);
+
+    }
+  }
+  if(arr.length === 0){
+    return true;
+  }else{
+    return false;
+  }
+
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
