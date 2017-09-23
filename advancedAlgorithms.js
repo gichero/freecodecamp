9d0f100587088882f4bfd21319c28a8e9c17edb5
Amos <amosgathungu@gmail.com>
2017-09-23 16:58:57 -0400

    algorithm practice (incomplete)

---
+++ advancedAlgorithms.js
@@ -0,0 +1,17 @@
+function checkCashRegister(price, cash, cashInDrawer){
+
+    let denominations = [
+        {name: 'penny', value: 0.01},
+        {name: 'nickle', value: 0.05},
+        {name: 'dime', value: 0.10},
+        {name: 'quater', value: 0.25},
+        {name: 'one', value: 1.00},
+        {name: 'five', value: 5.00},
+        {name: 'ten', value: 10.00},
+        {name: 'twenty', value: 20.00},
+        {name: 'fifty', value: 50.00},
+        {name: 'hundred', value: 100.00}
+    ];
+
+    
+}
