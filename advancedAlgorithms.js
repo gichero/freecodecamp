14e694b42433301f939fd8e983251f830bed9f41
Amos <amosgathungu@gmail.com>
2017-09-26 10:18:16 -0400

    algorithm practice

--- advancedAlgorithms.js
+++ advancedAlgorithms.js
@@ -1,50 +1,55 @@
 function checkCashRegister(price, cash, cashInDrawer){
 
-    let denominations = [
-        {name: 'penny', value: 0.01},
-        {name: 'nickle', value: 0.05},
-        {name: 'dime', value: 0.10},
-        {name: 'quater', value: 0.25},
-        {name: 'one', value: 1.00},
-        {name: 'five', value: 5.00},
-        {name: 'ten', value: 10.00},
-        {name: 'twenty', value: 20.00},
-        {name: 'fifty', value: 50.00},
-        {name: 'hundred', value: 100.00}
+  var denominations = [
+        {name: 'ONE HUNDRED', value: 100.00},
+        {name: 'TWENTY',      value: 20.00},
+        {name: 'TEN',         value: 10.00},
+        {name: 'FIVE',        value: 5.00},
+        {name: 'ONE',         value: 1.00},
+        {name: 'QUARTER',     value: 0.25},
+        {name: 'DIME',        value: 0.10},
+        {name: 'NICKEL',      value: 0.05},
+        {name: 'PENNY',       value: 0.01}
     ];
-    let change = cash - price;
 
-    let register = cashInDrawer.reduce((accumulator, nextValue)=>{
-        return accumulator + nextValue[1];
-    }, 0.0).toFixed(2);
+        var change = cash - price;
 
-    if(register < change){
-        return 'Insufficient Funds';
-    }else if(register === change){
-        return 'Closed';
-    }
+        var register = cashInDrawer.reduce(function(a, b) {
+            return a + b[1];
+        }, 0.0).toFixed(2);
 
-    let newArr = currency.reduce((acc, item, index)=>{
-        if(change >= item.value){
-            let currentValue = 0.0;
-            while(change >= item.value && cashInDrawer[index][1] >= item.value){
-                currentValue += nextValue;
-                change -= item.value;
-                change = Math.round(change*100)/100;
-                cashInDrawer[index][1] -= item.value;
-            }
-            acc.push([item.name], currentValue);
-            return acc;
-        }else{
-            return acc;
+        if(register < change){
+            return 'Insufficient Funds';
+        }else if(register == change){
+            return 'Closed';
         }
-    },[]);
 
-    if(newArr.length > 0 && change === 0){
-        return newArr;
-    }else{
-        return 'Insufficient Funds';
-    }
+        cashInDrawer = cashInDrawer.reverse();
+
+
+        var result = denominations.reduce(function(acc, next, index) {
+            if(change >= next.value){
+                var currentValue = 0.00;
+
+                while(change >= next.value && cashInDrawer[index][1] >= next.value){
+                    currentValue += next.value;
+                    change -= next.value;
+                    change = Math.round(change * 100) / 100;
+                    cashInDrawer[index][1] -= next.value;
+                }
+                acc.push([next.name, currentValue]);
+                return acc;
+            } else {
+                return acc;
+            }
+
+        }, []);
+
+        if(result.length > 0 && change === 0){
+            return result;
+        }else{
+            return 'Insufficient Funds';
+        }
 }
 
 checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
